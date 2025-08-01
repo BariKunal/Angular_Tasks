93b9f38b05236fb982c0e05ed78b3c114a6ca8a0
BariKunal <kunalbari751@gmail.com>
2025-08-01 15:29:35 +0530

    Initial commit with Angular Folder Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'First_Demo_App';
+  count = 0
+
+  Counter(val: String) {
+    if (val == 'plus') {
+      this.count = this.count + 1
+    }
+    else if (val == 'minus') {
+      if (this.count == 0) {
+        alert("Value Is Zero")
+      } else {
+        this.count = this.count - 1
+      }
+    }
+    else {
+      this.count = 0
+    }
+  }
 }

0fd51dde92c6b743746c1c5e46ddde760b8b65b2
BariKunal <kunalbari751@gmail.com>
2025-08-04 16:02:59 +0530

    Routing for the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { UserInfoComponent } from './User/user-info/user-info.component';
+// import { UserInfoComponent } from './User/user-info/user-info.component';
+// import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, UserInfoComponent],
+  imports: [CommonModule, RouterOutlet,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { UserInfoComponent } from './User/user-info/user-info.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'user',
+        component: UserInfoComponent,
+    },
+    { path: '', redirectTo: 'user', pathMatch: 'full' },
+    { path: '**', redirectTo: 'user' },
+    {
+        path: 'navbar',
+        component: NavBarComponent,
+    }
+];

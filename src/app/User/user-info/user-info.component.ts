e77e435e019809a4abe7f077df64cccf53d44a55
BariKunal <kunalbari751@gmail.com>
2025-08-04 11:07:40 +0530

    Standalone component

--- src/app/User/user-info/user-info.component.ts
+++ src/app/User/user-info/user-info.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-info',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.css'
